e9e93bbb39153bf2439cc1f720a612d751850c5e
Pedro Larroy <pedro.larroy.lists@gmail.com>
2022-12-20 00:36:53 -0800

    Fix lstm

--- classify.py
+++ classify.py
@@ -38,27 +38,32 @@ from torch.utils.data import Dataset, DataLoader
 from torch import nn
 import torchtext as tt
 from torchtext.data import Field
+import torch.optim
 
 from eda import run_eda
+import logging
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+logger = logging.getLogger(__name__)
 # TODO: Feel free to improve the model
 # Switch to softmax activation
 class EmailClassifier(nn.Module):
     def __init__(self, vocab_size, embed_size, hidden_size, num_class):
         super(EmailClassifier, self).__init__()
+        self.embed_size = embed_size
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(input_size=embed_size,
                             hidden_size=hidden_size,
+                            batch_first=True
                             )
         self.fc = nn.Linear(hidden_size, num_class)
 
     def forward(self, text):
         embeddings = self.embedding(text)
-        hidden_out = self.lstm(embeddings)
-        output = self.fc(hidden_out)
+        lstm_out, hidden = self.lstm(embeddings)
+        output = self.fc(hidden[0]).squeeze()
         return output
 
 
@@ -76,6 +81,7 @@ def analyse_data(data: list) -> None:
     See accompanying jupyter notebook "eda.ipynb"""
     #run_eda(pd.DataFrame(data))
     return None
+
 
 
 # Step #2: Define data fields
@@ -136,28 +142,57 @@ def extract_features(X_train: List, X_valid: List, TEXTFIELD: tt.data.Field,
         X_train.fields = fields_bug_workaround(X_train.fields)
         TEXTFIELD.build_vocab(X_train)
         LABELFIELD.build_vocab(X_train)
-        train_iter = tt.data.BucketIterator(X_train, batch_size=32, sort_key=lambda x: len(x.subject),
+        train_iter = tt.data.BucketIterator(X_train, batch_size=32, sort_key=lambda x: len(x.Subject),
                                                    device=device, sort=True, sort_within_batch=True)
 
     if X_valid:
         X_valid.fields = fields_bug_workaround(X_valid.fields)
-        val_iter = tt.data.BucketIterator(X_valid, batch_size=32, sort_key=lambda x: len(x.subject),
+        val_iter = tt.data.BucketIterator(X_valid, batch_size=32, sort_key=lambda x: len(x.Subject),
                                                  device=device, sort=True, sort_within_batch=True)
 
     return train_iter, val_iter, TEXTFIELD, LABELFIELD
 
 
+def text_to_tensor(tokens: List[str], vocab: tt.vocab.Vocab) -> torch.Tensor:
+    return torch.tensor([vocab[token] for token in tokens])
+
 # Step #4: Train model
 def train_model(classification_model: EmailClassifier, train_iter: tt.data.BucketIterator,
-                val_iter: tt.data.BucketIterator) -> EmailClassifier:
+                val_iter: tt.data.BucketIterator, epochs: int, x_vocab: tt.vocab.Vocab, label_vocab: tt.vocab.Vocab) -> EmailClassifier:
     """Create a training loop"""
+    opt = torch.optim.Adam(classification_model.parameters(), lr=0.001)
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-    train_iter.create_batches()
-    for batch in train_iter.batches:
-        for data_point in batch:
-            x = data_point.body
-            y = data_point.label
-
+    for epoch in range(epochs):
+        epoch_loss = 0
+        train_iter.create_batches()
+        for batch in train_iter.batches:
+            opt.zero_grad()
+            xs = []
+            ys = []
+            max_len = 0
+            for data_point in batch:
+                max_len = max(max_len, len(data_point.Body))
+            pad = x_vocab['<pad>']
+            for data_point in batch:
+                sentence = [x_vocab[x] for x in data_point.Body]
+                sentence.extend([pad]*(max_len-len(sentence)))
+                xs.append(sentence)
+                label = label_vocab[data_point.Label]
+                y = [0.0] * len(label_vocab)
+                y[label] = 1.0
+                ys.append(y)
+            xs_tensor = torch.tensor(xs)
+            ys_tensor = torch.tensor(ys)
+            pred = classification_model(xs_tensor)
+            #pred = classification_model.forward(x)
+            loss = loss_fn(pred, ys_tensor)
+            loss.backward()
+            opt.step()
+            #print("loss: {}".format(loss))
+            epoch_loss += loss
+        epoch_loss = epoch_loss / len(train_iter)
+        print("Epoch {}, loss {}".format(epoch, epoch_loss))
     return classification_model
 
 
@@ -167,6 +202,7 @@ def compute_metrics(classification_model: EmailClassifier, test_data: list) -> N
 
 
 def main(train_path: str, test_path: str) -> None:
+    logging.getLogger().setLevel(logging.INFO)
     ### Perform the following steps and complete the code
 
     ### Step #0: Load data
@@ -186,10 +222,12 @@ def main(train_path: str, test_path: str) -> None:
     train_iter, val_iter, TEXTFIELD, LABELFIELD = extract_features(train_ds, val_ds, TEXTFIELD, LABELFIELD)
     # Vocab size is not 5546 after the fix. Before it was 4 only special tokens, see comments in Changelog.
     vocab_size = len(TEXTFIELD.vocab.stoi)
+    print("Vocab size: {}".format(vocab_size))
 
     ### Step #4: Train model
+    epochs = 20
     classification_model = EmailClassifier(vocab_size=vocab_size, embed_size=128, hidden_size=128, num_class=4)
-    classification_model = train_model(classification_model, train_iter, val_iter)
+    classification_model = train_model(classification_model, train_iter, val_iter, epochs, TEXTFIELD.vocab, LABELFIELD.vocab)
 
     ### Step #5: Stand-alone Test data & Compute metrics
     test_data = load_data(test_path)
